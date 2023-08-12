0cf184402e24660512c9518c5e6bdb36e1e809e9
youngseok kim <tbvjdkfdir@gmail.com>
2023-08-12 13:13:27 +0900

    last-commit

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ let news = [];
 
 let a = "korea";
 
+let page = 1;
+
+let totalpages = 0;
+
 let searchlie = true;
 
 let selectall = document.querySelectorAll(".news-button button");
@@ -9,21 +13,33 @@ let selectall = document.querySelectorAll(".news-button button");
 let category = Array.from(selectall);
 
 const newsapi = async () => {
-  let url = new URL(
-    `https://newsapi.org/v2/everything?q=${a}&pagesize=10&language=es&apiKey=168047dcc3fa452aaeffcc94e399f6b2`
-  );
-  //   let header = new Headers({
-  //     "X-Api-Key": "168047dcc3fa452aaeffcc94e399f6b2",
-  //   });
+  try {
+    let url = new URL(
+      `https://newsapi.org/v2/everything?q=${a}&page=1&pagesize=5&language=es&apiKey=168047dcc3fa452aaeffcc94e399f6b2`
+    );
+    //   let header = new Headers({
+    //     "X-Api-Key": "168047dcc3fa452aaeffcc94e399f6b2",
+    //   });
 
-  let response = await fetch(url);
-  let data = await response.json();
+    let response = await fetch(url);
+    let data = await response.json();
 
-  news = data.articles;
-
-  console.log("This is data", data);
-  console.log(news);
-  uiupdate();
+    console.log(response);
+    console.log(data);
+    if (response.status == 200) {
+      if (data.articles.length == 0) {
+        throw new Error(`No search results`);
+      }
+      news = data.articles;
+      totalpages = Math.ceil(data.totalResults / 5);
+      uiupdate();
+    } else {
+      throw new Error(data.message);
+    }
+  } catch (error) {
+    console.log("에러:", error.message);
+    errorupdate(error.message);
+  }
 };
 
 newsapi();
@@ -56,14 +72,16 @@ let uiupdate = () => {
   document.getElementById("main-input").innerHTML = resulthtml;
 };
 
+const pagenation = () => {};
+
 let searchbar = () => {
   let search = "";
 
   searchlie = !searchlie;
 
   if (searchlie == false) {
-    search += `<input type=" text" placeholder="검색창">
-        <button id="search-button">Search</button>`;
+    search += `<input type=" text" placeholder="Search" id = "input-real" onkeypress ="enterkey(event)">
+        <button id="search-button" onclick="enterkey(event)">GO</button>`;
   }
 
   document.getElementById("search-box").innerHTML = search;
@@ -75,5 +93,20 @@ category.forEach((item) =>
 
 let topicevent = (event) => {
   a = event.target.textContent;
+
   newsapi();
+};
+
+function enterkey(event) {
+  if (event.key === "Enter" || event.target.id == `search-button`) {
+    a = document.getElementById("input-real").value;
+    newsapi();
+  }
+}
+
+const errorupdate = (message) => {
+  let errorhtml = `<div class="alert alert-danger" role="alert">
+  ${message}
+</div>`;
+  document.getElementById("main-input").innerHTML = errorhtml;
 };
